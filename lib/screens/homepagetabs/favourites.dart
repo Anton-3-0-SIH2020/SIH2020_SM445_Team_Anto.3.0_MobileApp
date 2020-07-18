6d5e416992d9b8812c6a0528ae794354c7ea712c
pratikbaid3 <pratikbaid3@gmail.com>
2020-07-19 00:14:33 +0530

    Added companies db assets

--- lib/screens/homepagetabs/favourites.dart
+++ lib/screens/homepagetabs/favourites.dart
@@ -79,154 +79,98 @@ class _FavouritesPageState extends State<FavouritesTab> {
         backgroundColor: Color(0xffF3F5F7),
         body: Column(
           children: <Widget>[
-            Row(
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    margin: EdgeInsets.only(left: 20, bottom: 15, top: 20),
-                    padding: EdgeInsets.only(
-                      left: 18,
-                      right: 8,
-                      top: 8,
-                      bottom: 8,
-                    ),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(15.5),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Color.fromRGBO(168, 192, 255, 0.16),
-                          spreadRadius: 5,
-                          blurRadius: 7,
-                          offset: Offset(0, 3), // changes position of shadow
-                        ),
-                      ],
-                    ),
-                    child: DropdownButton<String>(
-                      value: exchangeType,
-                      elevation: 16,
-                      underline: Container(height: 0),
-                      style: TextStyle(color: Colors.black45),
-                      icon: Icon(Icons.keyboard_arrow_down),
-                      onChanged: (String newValue) {
-                        setState(() {
-                          exchangeType = newValue;
-                        });
-                      },
-                      items: <String>['BSE', 'NSE', 'MC']
-                          .map<DropdownMenuItem<String>>((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(
-                            value,
-                          ),
-                        );
-                      }).toList(),
-                    ),
+            Container(
+              margin: EdgeInsets.only(left: 20, right: 20, bottom: 15, top: 20),
+              padding: EdgeInsets.only(
+                left: 21.5,
+                right: 8,
+                top: 8,
+                bottom: 8,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15.5),
+                boxShadow: [
+                  BoxShadow(
+                    color: Color.fromRGBO(168, 192, 255, 0.16),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: Offset(0, 3), // changes position of shadow
                   ),
-                ),
-                Expanded(
-                  flex: 3,
-                  child: Container(
-                    margin: EdgeInsets.only(
-                        left: 15, right: 20, bottom: 15, top: 20),
-                    padding: EdgeInsets.only(
-                      left: 21.5,
-                      right: 8,
-                      top: 8,
-                      bottom: 8,
-                    ),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(15.5),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Color.fromRGBO(168, 192, 255, 0.16),
-                          spreadRadius: 5,
-                          blurRadius: 7,
-                          offset: Offset(0, 3), // changes position of shadow
-                        ),
-                      ],
-                    ),
-                    //Search Bar
-                    child: TextFormField(
-                      onChanged: (value) async {
-                        //Filtering Content Logic
-                        List<BseCa> bseOriginalList = await bseList;
-                        List<NseCa> nseOriginalList = await nseList;
-                        List<McCa> mcOriginalList = await mcList;
+                ],
+              ),
+              //Search Bar
+              child: TextFormField(
+                onChanged: (value) async {
+                  //Filtering Content Logic
+                  List<BseCa> bseOriginalList = await bseList;
+                  List<NseCa> nseOriginalList = await nseList;
+                  List<McCa> mcOriginalList = await mcList;
 
-                        searchText = value;
-                        _debouncer.run(() {
-                          if (exchangeType == 'BSE') {
-                            setState(() {
-                              bseFilterList = bseOriginalList
-                                  .where((f) => (f.securityName
-                                          .toLowerCase()
-                                          .contains(value.toLowerCase()) ||
-                                      f.purpose
-                                          .toLowerCase()
-                                          .contains(value.toLowerCase())))
-                                  .toList();
-                            });
-                          } else if (exchangeType == 'NSE') {
-                            setState(() {
-                              nseFilterList = nseOriginalList
-                                  .where((f) => (f.symbol
-                                          .toLowerCase()
-                                          .contains(value.toLowerCase()) ||
-                                      f.purpose
-                                          .toLowerCase()
-                                          .contains(value.toLowerCase())))
-                                  .toList();
-                            });
-                          } else {
-                            setState(() {
-                              mcFilterList = mcOriginalList
-                                  .where((f) => (f.companyName
-                                          .toLowerCase()
-                                          .contains(value.toLowerCase()) ||
-                                      f.purpose
-                                          .toLowerCase()
-                                          .contains(value.toLowerCase())))
-                                  .toList();
-                            });
-                          }
-                        });
-                        print(searchText);
-                      },
-                      cursorColor: Color(0xff24213f),
-                      style: TextStyle(
-                        color: Color(0xff24213f),
-                      ),
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        //"SEARCH"
-                        hintText: 'Search', //'Search for a topic',
-                        suffixIcon: Icon(
-                          Icons.search,
-                          color: Colors.black38,
-                        ),
-                        hintStyle: TextStyle(
-                          color: Color(0xFF8d8d8d),
-                          fontSize: 14,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ),
+                  searchText = value;
+                  _debouncer.run(() {
+                    if (exchangeType == 'BSE') {
+                      setState(() {
+                        bseFilterList = bseOriginalList
+                            .where((f) => (f.securityName
+                                    .toLowerCase()
+                                    .contains(value.toLowerCase()) ||
+                                f.purpose
+                                    .toLowerCase()
+                                    .contains(value.toLowerCase())))
+                            .toList();
+                      });
+                    } else if (exchangeType == 'NSE') {
+                      setState(() {
+                        nseFilterList = nseOriginalList
+                            .where((f) => (f.symbol
+                                    .toLowerCase()
+                                    .contains(value.toLowerCase()) ||
+                                f.purpose
+                                    .toLowerCase()
+                                    .contains(value.toLowerCase())))
+                            .toList();
+                      });
+                    } else {
+                      setState(() {
+                        mcFilterList = mcOriginalList
+                            .where((f) => (f.companyName
+                                    .toLowerCase()
+                                    .contains(value.toLowerCase()) ||
+                                f.purpose
+                                    .toLowerCase()
+                                    .contains(value.toLowerCase())))
+                            .toList();
+                      });
+                    }
+                  });
+                  print(searchText);
+                },
+                cursorColor: Color(0xff24213f),
+                style: TextStyle(
+                  color: Color(0xff24213f),
+                ),
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  //"SEARCH"
+                  hintText: 'Search', //'Search for a topic',
+                  suffixIcon: Icon(
+                    Icons.search,
+                    color: Colors.black38,
+                  ),
+                  hintStyle: TextStyle(
+                    color: Color(0xFF8d8d8d),
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
                   ),
                 ),
-              ],
+              ),
             ),
 
             //TODO: LIST VIEW BUILDER
             Expanded(
               child: ListView.builder(
-                // Let the ListView know how many items it needs to build.
                 itemCount: favCompanies.length,
-                // Provide a builder function. This is where the magic happens.
-                // Convert each item into a widget based on the type of item it is.
                 itemBuilder: (context, index) {
                   final item = favCompanies[index];
 
